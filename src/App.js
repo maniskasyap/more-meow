fb54fd19a1dd43ee0bb59055109063b5d33fef51
manish <manis.kasyap@gmail.com>
2018-09-29 21:21:39 +0400

    showing more cats

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import brownz from "./brownz.jpeg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // catUrl: "https://purr.objects-us-west-1.dream.io/i/18882019_1932557806979479_8521838552350708560_n.jpg"
+      catUrl: brownz
+    };
+    this.getMoreCats = this.getMoreCats.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMoreCats();
+  }
+
+  getMoreCats() {
+    this.setState({
+      catUrl: brownz
+    });
+    const that = this;
+    fetch("https://aws.random.cat/meow")
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        that.setState({
+          catUrl: data.file
+        });
+      });
+  }
+
   render() {
+    const styles = {
+      backgroundImage: "url(" + this.state.catUrl + ")"
+      // backgroundSize: "cover"
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={styles} />
+        <button className="show-more" onClick={this.getMoreCats}>
+          MORE MEOWW !!!
+        </button>
       </div>
     );
   }
